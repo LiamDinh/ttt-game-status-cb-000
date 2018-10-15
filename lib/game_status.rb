0fb4b12b53778a09000fc901228b4371e7f134e4
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-15 19:46:47 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,7 +54,7 @@ end
 def winner(board)
   winning_side = []
   winning_side = won?(board)
-  if won?(board) == nil 
+  if won?(board) == nil
     return nil
   elsif board[winning_side[0]] == "X"
     return "X"
